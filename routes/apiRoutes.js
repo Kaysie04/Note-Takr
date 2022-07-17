20c7492695fc679c518743730ab508b6b7058150
Kaysie <kaysielandersonwork@gmail.com>
2022-07-17 13:15:04 -0500

    added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,64 @@
+// setup basic const for route file
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 
-router.post('/api/notes', (req, res) => {
-    console.log('req.body')
-    res.json(req.body)  
-  });
+// HANDLE GET REQUEST FROM CLIENT GET REQUEST
+
+router.get('/notes', (rew, res) => {
+  fs.readFile('./db/db.json', 'utf8', (error, data)=> {
+    if (error) {
+      res.status(500).json('500')
+      console.log('sucks to suck')
+    }
+    res.json(JSON.parse(data))
+  })
+})
+
+
+// HANDLE POST REQUEST FROM CLIENT POST REQUEST
+
+router.post('/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (error, data)=> {
+    if (error) {
+      res.status(500).json('500')
+      console.log('sucks to suck')
+    }
+    let newData = JSON.parse(data)
+     newData.push({...req.body, id:uuidv4()})
+    fs.writeFile('./db/db.json', JSON.stringify(newData), (error) => {
+      if (error) {
+        res.status(500).json('500')
+      }
+      res.json(req.body)
+    })
+  })
+})
 
 
 
+router.delete('/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (error, data)=> {
+    if (error) {
+      res.status(500).json('500')
+      console.log('sucks to suck')
+    }
+    let newData = JSON.parse(data)
+    for (let i = 0; i < newData.length; i++) {
+      if (newData[i].id === req.params.id) {
+        newData.splice(i,1)
+      }
 
-
-
-
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(newData), (error) => {
+      if (error) {
+        res.status(500).json('500')
+      }
+      res.json(req.body)
+    })
+  })
+})
 
 module.exports = router
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,21 @@
+
+// setup basic requirements for a route file
 const express = require('express')
 const router = express.Router()
 const path = require("path")
 
+
+// handle get request to display main page
+router.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+// handle get request to display notes page
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
-router.get("/index", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+
 
 module.exports = router
 
